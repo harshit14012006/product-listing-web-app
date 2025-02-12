49e793010009f4e33c44c0402a63724ffa4a28e2
harshit6002 <harshitemail2006@gmail.com>
2025-02-12 20:38:21 +0530

    done

---
+++ client/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  resolve: {
+    fallback: {
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      stream: require.resolve("stream-browserify"),
+      zlib: require.resolve("browserify-zlib"),
+      buffer: require.resolve("buffer/"),
+      process: require.resolve("process/browser"),
+    },
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"],
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        resolve: {
+          fullySpecified: false, // For Webpack 5 resolving issues
+        },
+      },
+    ],
+  },
+};
